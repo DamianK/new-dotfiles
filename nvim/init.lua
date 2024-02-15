815ac560a3fd9701c4b73a7c7a11eefa54a47204
Damian Kleiman <fliakleiman@gmail.com>
2024-02-15 09:16:25 -0300

    removed unused lines

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,11 +13,6 @@ require("copilot").setup({
 local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 
--- Appearance
-
-
--- Tree
-
 -- LSP
 local nvim_lsp = require('lspconfig')
  
