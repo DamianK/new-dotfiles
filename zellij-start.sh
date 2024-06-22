664b1205b107c230b51fd734b5f072380ce2de26
Damian Kleiman <fliakleiman@gmail.com>
2024-06-22 15:22:29 -0300

    script to start my Zellij tabs automatically

---
+++ zellij-start.sh
@@ -0,0 +1,6 @@
+!/usr/bin/bash
+zellij action new-tab -n NeoVim
+zellij action new-tab -n Term
+zellij action new-tab -n Server
+zellij action new-tab -n Midnight Commander
+zellij action new-tab -n Twitch
